d164616366e28fbfeadd802566fafc87048c2709
yr87u9i <gs1anasstea@hotmail.com>
2024-05-09 06:04:37 +0700

    demo project

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-function countWords(str) {
-  return str.split(" ").filter((word) => word !== "").length;
-}
+const chunk = (arr, size) =>
+  Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+    arr.slice(i * size, i * size + size),
+  );
