475e20023d6a6d2880a21d3e01cd10b9f4cc4bb0
yr87u9i <gs1anasstea@hotmail.com>
2024-05-16 17:05:13 +0700

    demo commit

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-const fibonacci = (n) => {
-  if (n <= 1) {
-    return n;
-  }
-  let prev = 0;
-  let curr = 1;
-  for (let i = 2; i <= n; i++) {
-    const next = prev + curr;
-    prev = curr;
-    curr = next;
-  }
-  return curr;
-};
+function fibonacci(n) {
+  if (n <= 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
